f7d6c8b4d7a3ddc8be4b882fe55be2e33f3e2a2e
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2022-10-31 21:27:56 -0300

    Update exemplo3_recursividade.cpp

--- exemplo3_recursividade.cpp
+++ exemplo3_recursividade.cpp
@@ -1,17 +1,18 @@
+/*Serie de fibonnaci com recursividade*/
 #include <stdio.h>
 
-int fribonnaci(int n){
+int fibonnaci(int n){
 	if(n==1 || n==2){
 		return 1;
 	}
 	else{
-		return(fribonnaci(n-1) + fribonnaci(n-2));
+		return(fibonnaci(n-1) + fibonnaci(n-2));
 	}
 }
 
 int main(){
 	int posicao;
-	printf("Digite a posicao do numero da serie de fribonnaci: ");
+	printf("Digite a posicao do numero da serie de fibonnaci: ");
 	scanf("%d",&posicao);
-	printf("O numero da posicao %d e: %d",posicao,fribonnaci(posicao));
+	printf("O numero da posicao %d e: %d",posicao,fibonnaci(posicao));
 }
