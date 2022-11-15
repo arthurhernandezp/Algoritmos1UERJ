37299304dad4e625e0c6ac37de2c906304e586ae
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2022-11-15 13:06:08 -0300

    Create ex3.c

---
+++ Codev/Variaveis/ex3.c
@@ -0,0 +1,4 @@
+/*
+Escreva uma função que receba como entrada um número n, um vetor m[], e um número k, e retorne a soma dos 
+números entre 1 e n que sejam múltiplos de alguns dos números m[1],.....,m[k]. [ O(n) ]
+*/
