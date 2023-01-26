90499e8987cade6c504ca5567de046fda5a3a3ea
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2023-01-26 13:32:56 -0300

    Update filaencad.c

--- filaencad.c
+++ filaencad.c
@@ -1,3 +1,19 @@
+/*
+1. Escreva uma função que calcule a soma das chaves dos nós de uma lista encadeada.
+2. Faça um programa que implemente uma função que calcule o número de nós de uma lista
+encadeada.
+3. Considere uma lista encadeada de valores inteiros e implemente uma função para
+retornar o número de nós da lista que possuem o campo info com valores maiores do que n.
+4. Implemente uma função que retorne o valor armazenado no último nó de uma lista
+encadeada de números inteiros.
+5. Considere listas de valores inteiros e implemente uma função que dada uma lista
+encadeada e um valor inteiro n, retire da lista todas as ocorrências de n retornando a lista
+resultante. Ao final, o programa deve imprimir a lista resultante.
+6. Implemente uma função que dados um valor e uma posição, a função insere o elemento
+valor na posição pos da lista simplesmente encadeada. Para isso, um novo nó deve ser
+alocado e ligado aos demais. O valor de pos é um número, que ser for 2, por exemplo, indica
+que o novo nó alocado será o segundo nó da lista.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct no{
@@ -120,6 +136,40 @@ void inserirFinal(no **n, int valor){
     }
 }
 
+int somaNos(no *n){
+    int somaTotal = 0;
+    no *aux;
+    aux = n;
+    while(aux != NULL){
+        somaTotal += aux->valor;
+        aux = aux->prox;
+    }
+    return somaTotal;
+}
+
+int calculoNos(no *n){
+    int totalnos = 0;
+    no *aux;
+    aux = n;
+    while(aux != NULL){
+        totalnos++;
+        aux = aux->prox;
+    }
+    return totalnos;
+}
+
+int procuraMaior(no *n, int valor){
+    int qtdMaior = 0;
+    no* aux;
+    aux = n;
+    while(aux != NULL){
+        if(aux->valor > valor) qtdMaior++;
+        aux= aux->prox;
+    }
+    return qtdMaior;
+}
+
+
 int main() {
     no *inicioE;
     printf("Antes: %d\n",listaVazia(inicioE));
@@ -137,10 +187,20 @@ int main() {
     inserirEsquerda(&inicioE,5);
     inserirEsquerda(&inicioE,16);
     pecorrer(inicioE);
+    printf("\nA soma dos nos e: %d",somaNos(inicioE));
+    printf("\nNumero de nos e: %d",calculoNos(inicioE));
+    printf("\nA quantidade de elementos maior que %d e: %d",10,procuraMaior(inicioE,10));
     removerTodosIguais(&inicioE,16);
     pecorrer(inicioE);
+    printf("\nA soma dos nos e: %d",somaNos(inicioE));
+    printf("\nNumero de nos e: %d",calculoNos(inicioE));
+    printf("\nA quantidade de elementos maior que %d e: %d",10,procuraMaior(inicioE,10));
+
     removerTodosIguais(&inicioE,5);
     pecorrer(inicioE);
+    printf("\nA soma dos nos e: %d",somaNos(inicioE));
+    printf("\nNumero de nos e: %d",calculoNos(inicioE));   
+    printf("\nA quantidade de elementos maior que %d e: %d",10,procuraMaior(inicioE,10));
 
 
     return 0;
